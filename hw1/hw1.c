9a564d3144f810eec137bb885332bf821cff0326
Duy Quoc Vu <vudu@login-students.ccs.neu.edu>
2017-01-18 23:12:52 -0500

    Adding stuff

--- hw1/hw1.c
+++ hw1/hw1.c
@@ -8,6 +8,7 @@
 #include <netdb.h>
 
 #define DEST_PORT 3700
+#define MAX_SIZE 64
 
 int getLength(char buffer[], int size) {
     int i = 0;
@@ -19,18 +20,54 @@ int getLength(char buffer[], int size) {
     return count;
 }
 
-void appendMessage(char *buffer, char *message, int size) {
+void appendString(char *buffer, char *message, int size) {
     int i = 0;
-    int messageLength = getLength(message, size);
+    int length = getLength(message, size);
 
     while(*(buffer + i) != '\0' && i < size - 1) {
-        if (length + i > size) {
-            printf("message too long");
+        if (length + i < size - 1) {
+            *(message + length + i) = *(buffer + i);
+        } else {
+            printf("too long: %d", getLength(message, size));
             exit(0);
         }
-        *(message + length + i) = *(buffer + i) 
+        i++;
+    }
+    *(message + length + i) = '\0';
+}
+
+int checkForNewline(char message[], int size) {
+    int i = 0;
+    while(i < size) {
+        if (message[i] == '\n') {
+            return 1;
+        }
+        i++;
+    }
+    return 0;
+}
+
+void addNewline(char *buffer) {
+    *(buffer + getLength(buffer, MAX_SIZE)) = '\n';
+}
+
+void resetArray(char *message, int size) {
+    int i = 0;
+    while(i < size) {
+        *(message + i) = '\0';
+        i++;
     }
 }
+
+void getSendString(char *result, char *sendString) {
+    int i = 0; //sine INFO  is 5 spaces to the numbers
+    while(sendString[i] != '\0') {
+        *(result + i) = *(sendString + i);
+        i++;
+       
+    }
+}
+
 
 int main(int argc, char* argv[]) {
     int port;
@@ -55,8 +92,11 @@ int main(int argc, char* argv[]) {
 
     int sockfd;
     struct sockaddr_in dest_addr;
-    char messageBuff[64] = "";
-    char buffer[64] = "";
+    char messageBuff[MAX_SIZE] = "";
+    char buffer[MAX_SIZE] = "";
+    char sendInfo[MAX_SIZE] = "";
+    int looping = 1; //flag to stop while loops
+    
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) {
@@ -69,10 +109,8 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
-    dest_addr.sin_family = AF_INET;          // host byte order
+    dest_addr.sin_family = AF_INET;     // host byte order
     dest_addr.sin_port = htons(port);   // short, network byte order
-    //dest_addr.sin_addr.s_addr = inet_addr(host);
-    //memset(&(dest_addr.sin_zero), '\0', 8);  // zero the rest of the struct
     memcpy(&dest_addr.sin_addr.s_addr, host->h_addr, host->h_length);
 
     
@@ -80,14 +118,59 @@ int main(int argc, char* argv[]) {
         printf("\nCannot open socket");
         return -1;
     } else {
-        if (recv(sockfd, buffer, 64, 0) >= 0) { 
-         
+        //wait for HELLO
+        while(looping) { 
+            if (recv(sockfd, buffer, MAX_SIZE, 0) >= 0) { 
+                appendString(buffer, messageBuff, MAX_SIZE);
+                if(checkForNewline(messageBuff, MAX_SIZE) != 0) {
+                    looping = 0;
+                    printf("\nReceive: %s\n", messageBuff);
+                    resetArray(messageBuff, MAX_SIZE); //reset messageBuff
+
+                    char sendString[] = "IAM ";
+                    appendString(nuId, sendString, MAX_SIZE);
+                    
+                    char result[getLength(sendString, MAX_SIZE)];
+                    getSendString(result, sendString);
+                    
+                    addNewline(result);
+                    send(sockfd, result, MAX_SIZE, 0);
+                    printf("\nSend: %s\n", result);
+                    send(sockfd, result, MAX_SIZE, 0);
+                }
+            }
         }
-        while(1) {
-            if (recv(sockfd, buffer, 64, 0) >= 0) {
-                int i = 0;
-                while(buffer[i] != '\0')
-                printf("\nmessage: %s\n", buffer);
+        looping = 1; 
+        //respone to INFOs
+        while(looping) {
+            if (recv(sockfd, buffer, MAX_SIZE, 0) >= 0) {
+                printf("\nbuffer: %s", buffer);
+                printf("\nmessage: %s", messageBuff);
+                appendString(buffer, messageBuff, MAX_SIZE);
+                if(checkForNewline(messageBuff, MAX_SIZE) != 0) {
+                    printf("\nReceive: %s\n", messageBuff);
+                    if (messageBuff[0]=='N' && messageBuff[1]=='U' && messageBuff[2]=='L' && messageBuff[3]=='L') {
+                        //NULL case
+                        printf("NUll here");
+                        resetArray(messageBuff, MAX_SIZE); //reset messageBuff
+                    } else if (messageBuff[0]=='K' && messageBuff[1]=='E' && messageBuff[2]=='Y') {
+                        //KEY case
+                        printf("\nDone");
+                        looping = 0;
+                    } else { 
+                        //REPLY case
+                        char sendString[] = "REPLY ";
+                        appendString((messageBuff + 5), sendString, MAX_SIZE);
+                        
+                        resetArray(messageBuff, MAX_SIZE); //reset messageBuff
+                        printf("\nSend: %s\n", sendString);
+                        char result[getLength(sendString, MAX_SIZE) + 1];
+                        getSendString(result, sendString);
+                        //printf("hhhh:%s", result);
+                        addNewline(result);
+                        send(sockfd, result, MAX_SIZE, 0);
+                    }
+                }
             }
         }
     }

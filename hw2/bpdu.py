c2f0bf1bac6d25766907d8aab35b760fb19544bf
Duy Quoc Vu <vudu@login-students.ccs.neu.edu>
2017-01-30 22:26:43 -0500

    done decoding

--- hw2/bpdu.py
+++ hw2/bpdu.py
@@ -20,7 +20,7 @@ def ether_ntoa(n):
 
 #convert string to hex:... notation
 def to_hex(n):
-    return '(0x' + "".join(("{:02x}".format(ord(c))) for c in n) + ')'
+    return '0x' + "".join(("{:02x}".format(ord(c))) for c in n)
 
 
 #ethernet packet
@@ -67,18 +67,19 @@ class BPDU:
         self.forward_delay = 15
 
     #decode the data
+    #make sure decode from 0 to 32 with the 3 timer values add up to a total of 38 bytes
     def decode(self, data):
         self.llc, self.protocol, self.version, self.type, self.flags, self.stp_root_pri, self.stp_root_mac, self.stp_root_cost, self.stp_bridge_pri, self.stp_bridge_mac, self.stp_port_id, self.stp_msg_age = struct.unpack('3s 2s s s s 2s 6s 4s 2s 6s 2s 2s', data[0:32])
 
     #print out the src and dst
     def print_packet(self):
-        print 'type: {}'.format(to_hex(self.type))
-        print 'flags: {}'.format(to_hex(self.flags))
-        print 'stp_root_pri: {}'.format(to_hex(self.stp_root_pri))
-        print 'stp_root_cost: {}'.format(to_hex(self.stp_root_cost))
-        print 'stp_bridge_pri: {}'.format(to_hex(self.stp_bridge_pri))
-        print 'stp_port_id: {}'.format(to_hex(self.stp_port_id))
-        print 'stp_msg_age: {}'.format(to_hex(self.stp_msg_age))
+        print 'type: {} # ({})'.format(int(to_hex(self.type), 0), to_hex(self.type))
+        print 'flags: {} # ({})'.format(int(to_hex(self.flags), 0), to_hex(self.flags))
+        print 'stp_root_pri: {} # ({})'.format(int(to_hex(self.stp_root_pri), 0), to_hex(self.stp_root_pri))
+        print 'stp_root_cost: {} # ({})'.format(int(to_hex(self.stp_root_cost), 0), to_hex(self.stp_root_cost))
+        print 'stp_bridge_pri: {} # ({})'.format(int(to_hex(self.stp_bridge_pri), 0), to_hex(self.stp_bridge_pri))
+        print 'stp_port_id: {} # ({})'.format(int(to_hex(self.stp_port_id), 0), to_hex(self.stp_port_id))
+        print 'stp_msg_age: {} # ({})'.format(int(to_hex(self.stp_msg_age), 0), to_hex(self.stp_msg_age))
         print 'stp_root_mac: {}'.format(ether_ntoa(self.stp_root_mac))
         print 'stp_bridge_mac: {}'.format(ether_ntoa(self.stp_bridge_mac)) 
 
@@ -89,12 +90,16 @@ if sys.argv[1] == 'decode':
     etherPacket = Ether()
     etherPacket.decode(buff)
     etherPacket.print_packet()
-    print ether_ntoa(etherPacket.dst)
+    
+    #check for destination address
     if (ether_ntoa(etherPacket.dst) == '01:80:c2:00:00:00'):
-        #if (struct.unpack())
+        #check for the LLC bits
+        #if (to_hex(struct.unpack('3s', etherPacket.data[0:3])[0]) == '0x424203'):
         bpduPacket = BPDU()
         bpduPacket.decode(etherPacket.data)
-        bpduPacket.print_packet()
+        if (to_hex(bpduPacket.llc) == '0x424203' and to_hex(bpduPacket.protocol) == '0x0000' and to_hex(bpduPacket.version) == '0x00' and to_hex(bpduPacket.flags) == '0x00' and to_hex(bpduPacket.type) == '0x00'):    
+            bpduPacket.print_packet()
+    
   
 
 if sys.argv[1] == 'encode':
